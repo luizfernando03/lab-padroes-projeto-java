54f1c4ded4a3a30bd99f15697a604e0345cc22d9
Luiz Fernando <luizfernandodemoura.2013@gmail.com>
2023-09-28 14:56:57 -0300

    criando pacage e classe Subsistema

---
+++ src/main/java/subsistema2/cep/CrmService.java
@@ -0,0 +1,13 @@
+package subssistema2.cep;
+
+public class CrmService {
+
+        private CrmService() {
+            super();
+        }
+
+    public static void gravarCliente(String nome, String cep, String cidade, String estado) {
+        System.out.println("Cliente salvo nop sistema de CRM");
+
+    }
+}
